7e936ac2b374cba4ac3aff5db7628964424876ae
Ari Brown <ari@aribrown.com>
2008-10-13 14:28:37 -0400

    it works! Need to build a custom EventContext so one can access params, but simple examples work!

--- lib/kompanjoner.rb
+++ lib/kompanjoner.rb
@@ -1,4 +1,5 @@
 require "rubygems"
+require "metaid"
 require "need"
 
 module Kompanjoner
@@ -10,6 +11,32 @@ module Kompanjoner
   #
   def self.version
     VERSION
+  end
+  
+  module Helpers
+    
+    ##
+    # sets the params to their values
+    # setting c, :params => [params, []] {...}
+    # does: c.params = params; blk.call; c.params = []
+    def with(hash)
+      hash.each do |key, value|
+        meta_def(key) { hash[key] }
+        meta_def("#{key}=") { |v| hash[key] = v }
+      end
+
+      return unless block_given?
+
+      result = yield
+
+      hash.each do |key, value|
+        meta_eval { remove_method(key) }
+        meta_eval { remove_method("#{key}=") }
+      end
+
+      result
+    end
+    
   end
 
 end
--- lib/kompanjoner/sinatra.rb
+++ lib/kompanjoner/sinatra.rb
@@ -1,4 +1,147 @@
 #  Created by ari on 2008-10-12.
 #  Copyleft (c) 2008. All pwnage reserved.
-
+require "rubygems"
+require "need"
 require "sinatra"
+
+need{ '../kompanjoner' }
+
+# Sinatra.application.lookup(faked_request)
+# requests are easy to fake
+# once you have the goods, just do @result.map `{|r| r.block.call }`
+
+# params in the env hash
+# @env["rack.input"]
+# @env["rack.url_scheme"]
+# @env["SCRIPT_NAME"]
+# @env["PATH_INFO"]
+# @env["SERVER_PORT"]
+# @env["REQUEST_METHOD"]
+# @env["QUERY_STRING"]
+# @env['CONTENT_LENGTH']
+# @env['CONTENT_TYPE']
+# @env["HTTP_HOST"]
+# @env["SERVER_NAME"]
+# @env["SCRIPT_NAME"]
+# @env["PATH_INFO"]
+# @env["rack.request.query_string"]
+# @env["rack.request.query_hash"]
+# @env["rack.request.query_string"]
+# @env["rack.request.query_hash"]
+# @env["rack.request.form_input"]
+# @env["rack.request.form_hash"]
+# @env["rack.request.form_vars"]
+# @env['HTTP_REFERER']
+# @env["rack.request.cookie_hash"]
+# @env["rack.request.cookie_string"]
+# @env["HTTP_COOKIE"]
+# @env["HTTP_X_REQUESTED_WITH"]
+# @env["HTTP_ACCEPT_ENCODING"]
+
+Kompanjoner::Sinatra = Module.new
+
+##
+# Fake Rack::Request class. The only difference is that
+# it includes two methods that were Sinatra's additions.
+# Pass this to Sinatra::application#lookup
+# 
+# @example Kompanjoner::Request.new 'REQUEST_METHOD' => 'GET', 'PATH_INFO' => '/'
+class Kompanjoner::Sinatra::Request
+  
+  ##
+  # @param hash<Hash{String => String}> a hash that
+  #   contains the values to be stored, and act as
+  #   the environment (that would otherwise be passed
+  #   from Rack)
+  def initialize(hash={})
+    (@hash = hash.dup).default = ''
+  end
+  
+  ##
+  # @see Hash
+  def [](key)
+    @hash[key]
+  end
+  
+  ##
+  # @see Hash
+  def []=(key, value)
+    @hash[key] = value
+  end
+  
+  ##
+  # Returns the hash
+  # @return [Hash{String => String}]
+  def env
+    @hash
+  end
+  
+  ##
+  # This is a shortcut for self['REQUEST_METHOD']
+  # 
+  # @see Rack::Request in Sinatra's code
+  def request_method
+    @hash['REQUEST_METHOD']
+  end
+  
+  ##
+  # This is a shortcut for self['PATH_INFO']
+  # 
+  # @see Rack::Request in Sinatra's code
+  def path_info
+    @hash['PATH_INFO']
+  end
+  
+end
+
+class ::Sinatra::Application
+  ##
+  # TODO
+  # replace Sinatra::EventContext with custom one
+  def dispatch(env)
+    request = Kompanjoner::Sinatra::Request.new(env)
+    context = Sinatra::EventContext.new(request, Rack::Response.new([], 200), {})
+    begin
+      returned =
+        catch(:halt) do
+          filters[:before].each { |f| context.instance_eval(&f) }
+          result = lookup(context.request)
+          context.route_params = result.params
+          context.response.status = result.status
+          context.reset!
+          [:complete, context.instance_eval(&result.block)]
+        end
+      body = returned.to_result(context)
+    rescue => e
+      request.env['sinatra.error'] = e
+      context.status(500)
+      raise if options.raise_errors && e.class != NotFound
+      result = (errors[e.class] || errors[Sinatra::ServerError]).invoke(request)
+      returned =
+        catch(:halt) do
+          [:complete, context.instance_eval(&result.block)]
+        end
+      body = returned.to_result(context)
+    end
+    body = '' unless body.respond_to?(:each)
+    body = '' if request.request_method.upcase == 'HEAD'
+    context.body = body.kind_of?(String) ? [*body] : body
+    context.finish
+  end
+  
+end
+
+module Kompanjoner::Sinatra
+  
+  ##
+  # Processes the path, method, and params sent in
+  # 
+  # @param path<String> the path, formatted like "/foo/bar?money=5"
+  # @param method<String> the request method, like "GET" or "POST" or "PUT", etc.
+  # 
+  # @return [String]
+  def self.process(path, method)
+    ::Sinatra.application.dispatch 'REQUEST_METHOD' => method, 'PATH_INFO' => path
+  end
+  
+end
